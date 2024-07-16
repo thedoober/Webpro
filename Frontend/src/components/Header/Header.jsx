cf4c5bd9536ff4c724a395dc8ce006ababe48ea8
Vivek Anand <thevivek9939@gmail.com>
2024-07-16 18:42:48 +0530

    This Project

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a delected array of dishes crafted with the finest ingredients and culinary expertise
+            Our misson is to satisfy your craving and elevate 
+            your dining experience. One delicious meal at a time.
+             </p>
+             <button>View menu</button>
+    </div>
+    </div>
+  )
+}
+
+export default Header
